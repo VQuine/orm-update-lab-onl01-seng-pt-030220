45fbba8971667c5301528de0291e44a1ca807668
VQuine <01010110.v@gmail.com>
2020-06-06 13:39:16 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -28,20 +28,47 @@ class Student
     DB[:conn].execute(sql)
   end
 
-  #written to avoid duplicate 
+  # #SAVEmethod written to avoid duplicates
+  #check if ID exists, if it does- UPDATE existing records with "new" object data
+  #if ID does not exist in database, INSERT the data into table
+  #difficult for me to understand and write from scratch, keep practicing, homegurl!
   def save
     if self.id #<--returns true if the object has an id persisted
-      UPDATE students SET columnName = new value WHERE id = value;
+      sql = <<-SQL
+      UPDATE students
+       SET name = ?, grade = ? 
+       WHERE id = ?
+       SQL
+       DB[:conn].execute(sql, self.name, self.grade, self.id) 
     else
       sql = <<-SQL
         INSERT INTO students (name, grade)
         VALUES (?, ?)
       SQL
       DB[:conn].execute(sql, self.name, self.grade) #<----repay to SQL what belongs to SQL and to Ruby what belongs to Ruby..or Caesar: Caesar, God: God! Mark12:17 Jesus is cool!
-      @id = DB[:conn].last_insert_row_id #<---way nicer than that SQL query: last_insert_rowid() stuff..repaying to RUBY!
-      
-    end
+      @id = DB[:conn].last_insert_row_id #<---way nicer than that SQL query: last_insert_rowid() stuff..repaying to RUBY!      
+    end    
+  end
+
+  def self.create(a_name, a_grade)
+    new_student = Student.new(a_name, a_grade)
+
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, new_student.name, new_student.grade) 
     
+    @id = DB[:conn].last_insert_row_id
+  end
+
+  def self.new_from_db(id_name_grade_array)
+    i = id_name_grade_array[0]
+    na = id_name_grade_array[1]
+    gr = id_name_grade_array[2]
+
+    new_student = Student.new(na, gr, i)   
   end
 
 
